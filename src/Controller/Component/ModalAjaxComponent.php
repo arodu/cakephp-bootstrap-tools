d27db2388113e102647f1cbab551fb0e7ec6a834
Alberto Rodriguez <arodu.dev@gmail.com>
2025-02-08 15:16:01 -0400

    Add JsonResponse class and enhance ModalAjaxComponent for improved AJAX handling

--- src/Controller/Component/ModalAjaxComponent.php
+++ src/Controller/Component/ModalAjaxComponent.php
@@ -1,12 +1,15 @@
 <?php
+
 declare(strict_types=1);
 
 namespace BootstrapTools\Controller\Component;
 
+use BootstrapTools\Http\JsonResponse;
 use Cake\Controller\Component;
 use Cake\Controller\ComponentRegistry;
 use Cake\Event\EventInterface;
 use Cake\Http\Response;
+use Cake\Log\Log;
 use Cake\Routing\Router;
 
 /**
@@ -14,18 +17,21 @@ use Cake\Routing\Router;
  */
 class ModalAjaxComponent extends Component
 {
+    const STRATEGY_HTML = 'html';
+    const STRATEGY_JSON = 'json';
+
     /**
      * Default configuration.
      *
      * @var array<string, mixed>
      */
     protected array $_defaultConfig = [
-        'strategy' => 'html', // html, json
+        'strategy' => self::STRATEGY_HTML,
         'ajaxClassNames' => 'Ajax',
+        'modalSuccessKey' => 'X-Modal-Ajax-Success',
     ];
 
-
-    public function beforeRender(EventInterface $event)
+    public function initialize(array $config): void
     {
         $controller = $this->getController();
         if ($controller->getRequest()->is('ajax')) {
@@ -33,21 +39,79 @@ class ModalAjaxComponent extends Component
         }
     }
 
-    public function success(): Response
+    public function beforeRender(EventInterface $event): void
     {
         $controller = $this->getController();
-        //$response = $controller->getResponse()->withHeader($this->getConfig('modalSuccessKey'), '1');
-        //$controller->setResponse($response);
-
-        return $controller->getResponse();
+        $controller->set('modalSuccessKey', $this->getConfig('modalSuccessKey'));
     }
 
-    public function error(): Response
+    public function send(bool $success, string $message = null, array $data = [], array $meta = [], int $status = 200): Response
+    {
+        if ($this->getConfig('strategy') === self::STRATEGY_HTML) {
+            return $this->handleHtmlResponse(
+                success: $success,
+                message: $message ?? 'Operation successful.',
+                data: $data,
+                meta: $meta,
+                status: $status
+            );
+        }
+
+        if ($this->getConfig('strategy') === self::STRATEGY_JSON) {
+            return $this->handleJsonResponse(
+                success: $success,
+                message: $message ?? 'Operation successful.',
+                data: $data,
+                meta: $meta,
+                status: $status
+            );
+        }
+
+        throw new \RuntimeException('Invalid strategy');
+    }
+
+
+    public function success(string $message = null, array $data = [], array $meta = [], int $status = 200): Response
+    {
+        return $this->send(true, $message, $data, $meta, $status);
+    }
+
+    public function error(string $message = null, array $data = [], array $meta = [], int $status = 400): Response
+    {
+        return $this->send(false, $message, $data, $meta, $status);
+    }
+
+    protected function handleHtmlResponse(bool $success, string $message, array $data = [], array $meta = [], int $status = 200): Response
     {
         $controller = $this->getController();
-        //$response = $controller->getResponse()->withHeader($this->getConfig('modalSuccessKey'), '0');
-        //$controller->setResponse($response);
+        $response = $controller->getResponse();
 
-        return $controller->getResponse();
+        return $response
+            ->withStatus($status)
+            ->withBody($controller->render()->getBody())
+            ->withHeader($this->getConfig('modalSuccessKey'), $success ? '1' : '0');
+    }
+
+    protected function handleJsonResponse(bool $success, string $message, array $data = [], array $meta = [], int $status = 200): Response
+    {
+        $controller = $this->getController();
+        try {
+            $html = (string) $this->getController()->render()->getBody();
+        } catch (\Exception $e) {
+            Log::error("HTML Render Error: {$e->getMessage()}");
+
+            throw $e;
+        }
+
+        $jsonResponse = new JsonResponse($controller->getResponse());
+
+        return $jsonResponse
+            ->success($success)
+            ->status($status)
+            ->message($message)
+            ->data($data)
+            ->meta($meta)
+            ->html($html)
+            ->build();
     }
 }
